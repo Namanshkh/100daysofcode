aea914b87e910702c5f02a1af925036f34b2f1dd
Namansh Khandelwal <83030396+Namanshkh@users.noreply.github.com>
2021-08-16 22:12:41 +0530

    Height of a genric tree

---
+++ Day-5/Heightoftree.java
@@ -0,0 +1,11 @@
+public static int height(Node node) {
+    int h = -1;
+
+    for (Node child : node.children) {
+      int ch = height(child);
+      h = Math.max(h, ch);
+    }
+    h += 1;
+
+    return h;
+  }
