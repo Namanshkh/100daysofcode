58d3f26c8c1fbc70852dd5022315687477e76ef3
Namansh Khandelwal <83030396+Namanshkh@users.noreply.github.com>
2021-10-01 11:24:35 +0530

    transform a cloned tree to a binary tree

---
+++ day-30/transformformcloned.java
@@ -0,0 +1,10 @@
+public static Node transBackFromLeftClonedTree(Node node){
+    if(node == null){
+      return null;
+    }
+
+    node.left = transBackFromLeftClonedTree(node.left.left);
+    node.right = transBackFromLeftClonedTree(node.right);
+
+    return node;
+  }
